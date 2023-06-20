c2ffac5f74ece3d15aa7aee92d86dc78a98447b0
LuanCavicchioli <luan.caaziani@senacsp.edu.br>
2023-06-20 15:19:54 -0300

    Primeiro Commit

---
+++ Atividade4Notas/Atividade4Notas/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade4Notass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Media de 4 notas
+            double nota1, nota2, nota3, nota4, total, media;
+
+            Console.WriteLine("///////////////////////////");
+            Console.WriteLine("|     Calcular Media     |");
+            Console.WriteLine("///////////////////////////");
+
+            //Atribuir Valor As 4 Notas
+            Console.Write("Digite a primeira nota :");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota:");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a terceira nota:");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quarta nota:");
+            nota4 = Convert.ToDouble(Console.ReadLine());
+            //Atribuir os resultados a um total e calcular media
+            total = (nota1 + nota2) + (nota2 + nota3);
+
+            media = total / 4;
+            //Verificação da media
+            if (media >= 7)
+            {
+                Console.WriteLine("Voce foi aprovado :D, sua media é:  " + media);
+            }
+            else
+            {
+                Console.WriteLine("Voce foi reprovado :( , sua media é : " + media);
+            }
+
+        }
+    }
+}
---
+++ AtividadeDeCalculo4Nota/AtividadeDeCalculo4Nota/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, numero3, numero4, media;
+
+            Console.WriteLine("///////////////////////////");
+            Console.WriteLine("|     Calcular Media     |");
+            Console.WriteLine("///////////////////////////");
+            Console.Write("Digite o numero 1 :");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o numero 2 :");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o numero 3 :");
+            numero3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o numero 4 :");
+            numero4 = Convert.ToInt32(Console.ReadLine());
+
+            media = (numero1 + numero2 + numero3 + numero4) / 4;
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("|" + "Sua media é :" + media + "|");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+        }
+    }
+}
